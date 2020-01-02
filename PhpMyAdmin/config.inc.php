6c96c007dffc9e88114255225be427e72405f3f7
Francestu96 <francestu96@gmail.com>
2020-01-02 10:04:10 -0800

    php add

---
+++ PhpMyAdmin/config.inc.php
@@ -0,0 +1,11 @@
+<?php
+$i=0;
+$i++;
+
+$cfg['Servers'][$i]['auth_type'] = 'config';
+$cfg['Servers'][$i]['host'] = '172.17.0.2';
+$cfg['Servers'][$i]['user'] = 'root';
+$cfg['Servers'][$i]['password'] = 'user';
+$cfg['Servers'][$i]['compress'] = false;
+$cfg['Servers'][$i]['AllowNoPassword'] = false;
+
