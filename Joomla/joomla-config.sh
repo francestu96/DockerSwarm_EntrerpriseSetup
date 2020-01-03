27802e40b93a1bbe10727662d44fbd036d04c8cb
Francestu96 <francestu96@gmail.com>
2020-01-03 07:02:11 -0800

    working

--- Joomla/joomla-config.sh
+++ Joomla/joomla-config.sh
@@ -3,26 +3,18 @@
 JUSERID=1
 JUSERNAME="admin"
 JUSEREMAIL="admin@localhost.com"
-JUSERPASS="user"
+JUSERPASS="admin"
 DBPREFIX="prfx_"
 
 
 rm -f /var/www/html/index.html
-sed -i "s/DB_HOST/${DB_HOST}/g" configuration.php
-sed -i "s/DB_USER/${DB_USER}/g" configuration.php
-sed -i "s/DB_PASSWORD/${DB_PASSWORD}/g" configuration.php
-sed -i "s/DB_NAME/${DB_NAME}/g" configuration.php
-
-#sed -i "s/\$user = ''/\$user = '${DB_USER}'/" installation/configuration.php-dist
-#sed -i "s/\$password = ''/\$password = '${DB_PASSWORD}'/" installation/configuration.php-dist
-#sed -i "s/\$db = ''/\$db = '${DB_NAME}'/" installation/configuration.php-dist
-#sed -i "s/\$host = ''/\$host = '${DB_HOST}'/" installation/configuration.php-dist
-#sed -i "s/\$dbprefix = 'jos_'/\$dbprefix = '${DBPREFIX}'/" installation/configuration.php-dist
-#sed -i "s/\$tmp_path = '\/tmp'/\$tmp_path = '\/var\/www\/tmp'/" installation/configuration.php-dist
-#sed -i "s/\$log_path = '\/var\/logs'/\$log_path = '\/var\/www\/logs'/" installation/configuration.php-dist
-#sed -i "s/\$cache_handler = 'file'/\$cache_handler = ''/" installation/configuration.php-dist
+sed -i "s/\$host = 'localhost'/\$host = '${DB_HOST}'/" installation/configuration.php-dist
+sed -i "s/\$user = ''/\$user = '${DB_USER}'/" installation/configuration.php-dist
+sed -i "s/\$password = ''/\$password = '${DB_PASSWORD}'/" installation/configuration.php-dist
+sed -i "s/\$db = ''/\$db = '${DB_NAME}'/" installation/configuration.php-dist
+sed -i "s/\$dbprefix = 'jos_'/$\dbprefix = '${DBPREFIX}'/" installation/configuration.php-dist
 sed -i "s/#__/${DBPREFIX}/" installation/sql/mysql/joomla.sql
-#mv installation/configuration.php-dist configuration.php
+cp installation/configuration.php-dist configuration.php
 
 cat installation/sql/mysql/joomla.sql | mysql -h $DB_HOST -u $DB_USER --password=$DB_PASSWORD  $DB_NAME
 JPASS="$(echo -n "$JUSERPASS" | md5sum | awk '{ print $1 }' )"
@@ -30,3 +22,4 @@ echo "INSERT INTO \`${DBPREFIX}users\` (\`id\`, \`name\`, \`username\`, \`email\
 echo "INSERT INTO \`${DBPREFIX}user_usergroup_map\` (\`user_id\`, \`group_id\`) VALUES ('${JUSERID}', '8');" | mysql -h $DB_HOST -u $DB_USER --password=$DB_PASSWORD $DB_NAME
 JUSERINC=$((JUSERID+1))
 echo "ALTER TABLE \`${DBPREFIX}users\` auto_increment = ${JUSERINC};" | mysql -h $DB_HOST -u $DB_USER --password=$DB_PASSWORD $DB_NAME
+rm -d -r installation
--- PhpMyAdmin/config.inc.php
+++ PhpMyAdmin/config.inc.php
@@ -3,7 +3,7 @@ $i=0;
 $i++;
 
 $cfg['Servers'][$i]['auth_type'] = 'config';
-$cfg['Servers'][$i]['host'] = '172.17.0.2';
+$cfg['Servers'][$i]['host'] = '172.17.0.4';
 $cfg['Servers'][$i]['user'] = 'root';
 $cfg['Servers'][$i]['password'] = 'user';
 $cfg['Servers'][$i]['compress'] = false;
