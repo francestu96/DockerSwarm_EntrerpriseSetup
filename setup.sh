ec4b95c9abe5811e4d4d91112cc6f5672ee6e747
Francestu96 <francestu96@gmail.com>
2020-01-19 07:57:03 -0800

    fix

--- setup.sh
+++
@@ -1,7 +0,0 @@
-docker build -t custom-joomla Joomla/
-docker build -t custom-proxy Proxy/
-docker build -t custom-phpmyadmin PhpMyAdmin/
-docker build -t custom-prometheus Prometheus/
-docker build -t custom-grafana Grafana/
-
-docker stack deploy my-swarm -c docker-compose.yml
