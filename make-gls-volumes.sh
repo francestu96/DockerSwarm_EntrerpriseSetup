9b59740776c43109ea0f2d8ca1746dda760e3d7a
Francestu96 <francestu96@gmail.com>
2020-01-19 09:04:32 -0800

    add joomla volume files

--- make-gls-volumes.sh
+++ make-gls-volumes.sh
@@ -2,4 +2,8 @@ mkdir -p /gfs/joomla-volume/administrator
 mkdir -p /gfs/joomla-volume/components
 mkdir -p /gfs/joomla-volume/images
 
+cp -R -n Joomla/administrator/. /gfs/joomla-volume/administrator
+cp -R -n Joomla/components/. /gfs/joomla-volume/components
+cp -R -n Joomla/images/. /gfs/joomla-volume/images
+
 chown -R www-data:www-data /gfs/joomla-volume
